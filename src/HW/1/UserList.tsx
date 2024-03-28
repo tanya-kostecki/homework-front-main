5c1ecb9592f78bd3aa581109d2ad0fbc9b589484
tanya-kostecki <tanya.pavlova1294@gmail.com>
2024-03-28 15:52:56 +0100

    1

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -20,10 +20,11 @@ export const UserList = (props: UserListPropsType) => {
       <h2>User List:</h2>
 
       <ul>
-       {props.users.map((user) => (
-          <li key={user.id} id={`hw01-user-${user.id}`}>
-            <strong>{user.address.street}, {user.address.city}</strong>
-          </li>
+       {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+         <li key={user.id} id={`hw01-user-${user.id}`}>
+           <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+           {user.address.street}, {user.address.city}
+         </li>
        ))}
       </ul>
     </div>
